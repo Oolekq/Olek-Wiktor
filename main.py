382164e23a34bf3825db01b2a1ba124b6c4b4854
wiktor-m-dev <roktivv123@gmail.com>
2026-03-18 18:36:40 +0100

    fixed the wrong command in config class

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import BaseModel, Field, ConfigDict, EmailStr, ValidationError, field_validator, model_validator
 from decimal import Decimal
 from enum import Enum
@@ -10,8 +10,7 @@ from pydantic.alias_generators import to_camel
 class GlobalConfig(BaseSettings):
     strict_mode: bool = False
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_file=".env")
 
 settings = GlobalConfig() 
 
